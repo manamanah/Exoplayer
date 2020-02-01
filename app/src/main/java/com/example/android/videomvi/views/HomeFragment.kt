5ee91116712efabd332c2498a52856ca1cf27e0c
Birgit Schönauer <bschoenauer.mmt-b2016@fh-salzburg.ac.at>
2020-02-01 12:58:20 +0100

    feat: Add basic views and navigation

---
+++ app/src/main/java/com/example/android/videomvi/views/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.android.videomvi.views
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.example.android.videomvi.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.play_audio_button.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToPlayerFragment())
+        }
+
+        view.play_video_button.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToPlayerFragment())
+        }
+
+        return view
+    }
+}
